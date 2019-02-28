4c76fcdcf74bbf93ec24834f15f187d92192fd1c
sorinirimies <mihaiirimies@gmail.com>
2019-02-28 17:03:04 +0100

    bottom menu handling

--- app/src/main/java/de/graphql/movies/MainActivity.kt
+++ app/src/main/java/de/graphql/movies/MainActivity.kt
@@ -2,11 +2,44 @@ package de.graphql.movies
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.transaction
+import de.graphql.movies.ui.movies.MoviesFragment
+import de.graphql.movies.ui.tvshows.TvShowsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.transaction {
+                add(
+                    R.id.frag_container, MoviesFragment.newInstance(),
+                    MoviesFragment.TAG_MOVIES
+                )
+            }
+        }
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_movies -> supportFragmentManager.transaction {
+                    replace(
+                        R.id.frag_container,
+                        MoviesFragment.newInstance(),
+                        MoviesFragment.TAG_MOVIES
+                    )
+                }
+                R.id.action_tv_shows -> {
+                    supportFragmentManager.transaction {
+                        replace(
+                            R.id.frag_container,
+                            TvShowsFragment.newInstance(),
+                            TvShowsFragment.TAG_TV_SHOWS
+                        )
+                    }
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
     }
 }
