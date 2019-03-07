eb93390912eaf5b81c886dd835a24e74636d7db9
sorinirimies <mihaiirimies@gmail.com>
2019-03-07 16:06:39 +0100

    provide model and netowrk injected dependencies

--- app/src/main/java/de/graphql/movies/injection/networkModule.kt
+++ app/src/main/java/de/graphql/movies/injection/networkModule.kt
@@ -1,14 +1,15 @@
 package de.graphql.movies.injection
 
 import com.apollographql.apollo.ApolloClient
+import de.graphql.movies.utils.ApolloManager
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import timber.log.Timber
 
-private const val BASE_URL = "http://localhost:9596/graphql"
-
+private const val BASE_URL = "http://10.0.2.2:9596/graphql"
+const val TMDB_BASE_IMAGE_URL = "https://image.tmdb.org/t/p/"
 val networkModule: Module = module {
     single {
         OkHttpClient.Builder().addInterceptor(
@@ -20,5 +21,8 @@ val networkModule: Module = module {
             .serverUrl(BASE_URL)
             .okHttpClient(get())
             .build()
+    }
+    single {
+        ApolloManager(get())
     }
 }
--- app/src/main/java/de/graphql/movies/injection/viewModelModule.kt
+++ app/src/main/java/de/graphql/movies/injection/viewModelModule.kt
@@ -2,12 +2,10 @@ package de.graphql.movies.injection
 
 import de.graphql.movies.ui.movies.MoviesDetailsViewModel
 import de.graphql.movies.ui.movies.MoviesViewModel
-import de.graphql.movies.ui.tvshows.TvShowsViewModel
-import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelsModule = module {
-    viewModel { MoviesViewModel() }
-    viewModel { TvShowsViewModel() }
+    viewModel { MoviesViewModel(get()) }
     viewModel { MoviesDetailsViewModel() }
 }
