9e2596bd719be40a0d2661ae82da0d82fb6fda7c
sorinirimies <mihaiirimies@gmail.com>
2019-03-08 21:11:08 +0100

    utils

--- app/src/main/java/de/graphql/movies/utils/movieUtils.kt
+++ app/src/main/java/de/graphql/movies/utils/movieUtils.kt
@@ -1,7 +1,41 @@
 package de.graphql.movies.utils
 
 import de.graphql.movies.MoviesQuery
+import de.graphql.movies.MoviesWithMainActorsQuery
 import de.graphql.movies.model.MovieDetails
 
 fun MoviesQuery.Movie.toMovieDetails() =
-    MovieDetails(this.title(), this.overview(), this.release_date(), this.vote_average(), this.poster_path())
+    MovieDetails(
+        this.vote_count(),
+        this.id(),
+        this.video(),
+        this.vote_average(),
+        this.title(),
+        this.popularity(),
+        this.poster_path(),
+        this.backdrop_path(),
+        this.adult(),
+        this.overview(),
+        this.release_date()
+    )
+
+fun MoviesWithMainActorsQuery.MoviesWithMainActor.toMovieDetails() =
+    MovieDetails(
+        this.vote_count(),
+        this.id(),
+        this.video(),
+        this.vote_average(),
+        this.title(),
+        this.popularity(),
+        this.poster_path(),
+        this.backdrop_path(),
+        this.adult(),
+        this.overview(),
+        this.release_date(),
+        this.cast_id(),
+        this.character(),
+        this.actorId(),
+        this.name(),
+        this.order(),
+        this.profile_path()
+    )
