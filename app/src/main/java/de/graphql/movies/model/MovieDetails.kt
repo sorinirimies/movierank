2819d5708862e6e7efd36a93a6839164f8e62617
sorinirimies <mihaiirimies@gmail.com>
2019-03-08 21:06:55 +0100

    enhance model to become mapper for multiple queries

--- app/src/main/java/de/graphql/movies/model/MovieDetails.kt
+++ app/src/main/java/de/graphql/movies/model/MovieDetails.kt
@@ -5,9 +5,21 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 class MovieDetails(
+    val vote_count: Int,
+    val id: Int,
+    val video: Boolean,
+    val vote_average: Double,
     val title: String,
-    val description: String,
-    val date: String,
-    val voteAverage: Double,
-    val posterUrl: String? = ""
+    val popularity: Double,
+    val poster_path: String?,
+    val backdrop_path: String? = "",
+    val adult: Boolean,
+    val overview: String,
+    val release_date: String,
+    val cast_id: Int? = null,
+    val character: String? = null,
+    val actorId: Int? = null,
+    val name: String? = null,
+    val order: Int? = null,
+    val profile_path: String? = null
 ) : Parcelable
