38e2902ea9ae3966d7004390f19537faf0ba7d27
sorinirimies <mihaiirimies@gmail.com>
2019-03-08 21:10:55 +0100

    load movies with actors based on year request, also use MovieDetails as a general mapper

--- app/src/main/java/de/graphql/movies/ui/movies/MoviesViewModel.kt
+++ app/src/main/java/de/graphql/movies/ui/movies/MoviesViewModel.kt
@@ -3,11 +3,14 @@ package de.graphql.movies.ui.movies
 import androidx.lifecycle.MutableLiveData
 import de.graphql.movies.CoroutineViewModel
 import de.graphql.movies.MoviesQuery
+import de.graphql.movies.MoviesWithMainActorsQuery
+import de.graphql.movies.model.MovieDetails
 import de.graphql.movies.utils.ApolloManager
+import de.graphql.movies.utils.toMovieDetails
 
 class MoviesViewModel(private val apolloManager: ApolloManager) : CoroutineViewModel() {
 
-    val moviesLiveData: MutableLiveData<MovieItemsState> = MutableLiveData()
+    val moviesLiveData: MutableLiveData<List<MovieDetails>> = MutableLiveData()
     val errorMessage: MutableLiveData<String> = MutableLiveData()
 
     fun loadMovies(listSize: Int) {
@@ -16,12 +19,37 @@ class MoviesViewModel(private val apolloManager: ApolloManager) : CoroutineViewM
             .build(),
             onResponse = {
                 it.data()?.let { data ->
-                    moviesLiveData.postValue(MovieItemsState(data.movies()))
+                    moviesLiveData.postValue(data.movies()
+                        .asSequence()
+                        .map { movie ->
+                            movie.toMovieDetails()
+                        }.toList()
+                    )
                 }
             },
 
             onFailure = {
                 errorMessage.postValue(it.message)
+            })
+    }
+
+    fun loadMoviesWithMainActor(year: Int?) {
+        apolloManager.enqueueQuery(query = MoviesWithMainActorsQuery.builder()
+            .year(year)
+            .build(),
+            onResponse = {
+                it.data()?.let { data ->
+                    moviesLiveData.postValue(
+                        data.moviesWithMainActors()
+                            .asSequence()
+                            .map { moviesWithMainActor ->
+                                moviesWithMainActor.toMovieDetails()
+                            }.toList()
+                    )
+                }
+            },
+            onFailure = {
+
             })
     }
 }
