dc5f4941a441c054f2e59b022ecc7d0e355ea76c
sorinirimies <mihaiirimies@gmail.com>
2019-03-08 21:09:37 +0100

    extend with actor information

--- app/src/main/java/de/graphql/movies/ui/movies/MoviesAdapter.kt
+++ app/src/main/java/de/graphql/movies/ui/movies/MoviesAdapter.kt
@@ -5,16 +5,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
-import de.graphql.movies.MoviesQuery
 import de.graphql.movies.R
 import de.graphql.movies.injection.TMDB_BASE_IMAGE_URL
+import de.graphql.movies.model.MovieDetails
 import de.graphql.movies.model.TmdbImgSize
 import kotlinx.android.synthetic.main.listitem_movie.view.*
 
-typealias  MovieSelected = (movieListItem: MoviesQuery.Movie) -> Unit
+
+typealias  MovieSelected = (movieListItem: MovieDetails) -> Unit
 
 class MoviesAdapter(private val movieSelected: MovieSelected) : RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder>() {
-    private val movieListItems: ArrayList<MoviesQuery.Movie> = ArrayList()
+
+    private val movieListItems: ArrayList<MovieDetails> = ArrayList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder =
         MoviesViewHolder(
@@ -28,13 +30,13 @@ class MoviesAdapter(private val movieSelected: MovieSelected) : RecyclerView.Ada
     override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) =
         holder.bind(movieListItems[position], movieSelected)
 
-    fun addItem(item: MoviesQuery.Movie) {
-        if (movieListItems.map { it.title() }.contains(item.title())) return
+    fun addItem(item: MovieDetails) {
+        if (movieListItems.map { it.title }.contains(item.title)) return
         movieListItems.add(item)
         notifyItemInserted(movieListItems.indexOf(item))
     }
 
-    fun addItems(items: List<MoviesQuery.Movie>) {
+    fun addItems(items: List<MovieDetails>) {
         movieListItems.clear()
         movieListItems.addAll(items)
         notifyDataSetChanged()
@@ -45,7 +47,7 @@ class MoviesAdapter(private val movieSelected: MovieSelected) : RecyclerView.Ada
         notifyDataSetChanged()
     }
 
-    fun removeItem(item: MoviesQuery.Movie) {
+    fun removeItem(item: MovieDetails) {
         val position = movieListItems.indexOf(item)
         movieListItems.removeAt(position)
         notifyItemRemoved(position)
@@ -60,17 +62,31 @@ class MoviesAdapter(private val movieSelected: MovieSelected) : RecyclerView.Ada
         private val contMovieItem = view.contMovieItem
         private val imgMovieTitle = view.imgItemMoviePoster
         private val tvMovieTitle = view.tvItemMovieTitle
-        private val tvMovieScore = view.tvItemMovieRating
+        private val tvItemMovieVoteCount = view.tvItemMovieVoteCount
+        private val tvMovieVoteAverage = view.tvItemMovieVoteAverage
+        private val tvMovieActorName = view.tvMovieActorName
         private val tvMovieYear = view.tvItemMovieYear
+        private val tvMoviePopularity = view.tvItemMoviePopularity
+        private val imgMovieMainActor = view.imgMovieMainActor
 
-        fun bind(movieListItem: MoviesQuery.Movie, movieSelected: MovieSelected) {
+        fun bind(movieListItem: MovieDetails, movieSelected: MovieSelected) {
             Picasso.get()
-                .load("$TMDB_BASE_IMAGE_URL${TmdbImgSize.IMG_185.size}${movieListItem.poster_path()}")
+                .load("$TMDB_BASE_IMAGE_URL${TmdbImgSize.IMG_342.size}${movieListItem.poster_path}")
                 .placeholder(R.drawable.ic_movies)
                 .into(imgMovieTitle)
-            tvMovieTitle.text = movieListItem.title()
-            tvMovieScore.text = "${movieListItem.popularity()}"
-            tvMovieYear.text = movieListItem.release_date()
+            imgMovieMainActor.visibility = View.GONE
+            movieListItem.profile_path?.let {
+                imgMovieMainActor.visibility = View.VISIBLE
+                Picasso.get()
+                    .load("$TMDB_BASE_IMAGE_URL${TmdbImgSize.IMG_ORIGINAL.size}$it")
+                    .into(imgMovieMainActor)
+            }
+            tvMovieActorName.text = movieListItem.name
+            tvMovieTitle.text = movieListItem.title
+            tvItemMovieVoteCount.text = "${movieListItem.vote_count}"
+            tvMovieYear.text = movieListItem.release_date
+            tvMoviePopularity.text = "${movieListItem.popularity}"
+            tvMovieVoteAverage.text = "${movieListItem.vote_average}"
             contMovieItem.setOnClickListener { movieSelected.invoke(movieListItem) }
         }
     }
