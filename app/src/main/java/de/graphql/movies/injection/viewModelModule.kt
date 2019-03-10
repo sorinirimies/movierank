6965031adad8714d05306f6030796a7c60bc4a45
sorinirimies <mihaiirimies@gmail.com>
2019-03-10 22:13:06 +0100

    provide ApoppoManager in constructor and add missing ViewModel depen.

--- app/src/main/java/de/graphql/movies/injection/viewModelModule.kt
+++ app/src/main/java/de/graphql/movies/injection/viewModelModule.kt
@@ -1,11 +1,13 @@
 package de.graphql.movies.injection
 
-import de.graphql.movies.ui.movies.MoviesDetailsViewModel
+import de.graphql.movies.ui.movies.MovieDetailsViewModel
 import de.graphql.movies.ui.movies.MoviesViewModel
+import de.graphql.movies.ui.tvshows.TvShowsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelsModule = module {
     viewModel { MoviesViewModel(get()) }
-    viewModel { MoviesDetailsViewModel() }
+    viewModel { MovieDetailsViewModel(get()) }
+    viewModel { TvShowsViewModel() }
 }
