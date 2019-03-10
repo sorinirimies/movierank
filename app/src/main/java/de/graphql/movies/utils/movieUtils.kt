85e085019e035ed1426f4484750a79b385efb85b
sorinirimies <mihaiirimies@gmail.com>
2019-03-10 22:00:17 +0100

    add mapper for CastItem when getting Graphql Cast query response

--- app/src/main/java/de/graphql/movies/utils/movieUtils.kt
+++ app/src/main/java/de/graphql/movies/utils/movieUtils.kt
@@ -1,11 +1,13 @@
 package de.graphql.movies.utils
 
+import de.graphql.movies.MovieCastQuery
 import de.graphql.movies.MoviesQuery
 import de.graphql.movies.MoviesWithMainActorsQuery
-import de.graphql.movies.model.MovieDetails
+import de.graphql.movies.model.CastItem
+import de.graphql.movies.model.MovieItem
 
-fun MoviesQuery.Movie.toMovieDetails() =
-    MovieDetails(
+fun MoviesQuery.Movie.toMovieItem() =
+    MovieItem(
         this.vote_count(),
         this.id(),
         this.video(),
@@ -19,8 +21,8 @@ fun MoviesQuery.Movie.toMovieDetails() =
         this.release_date()
     )
 
-fun MoviesWithMainActorsQuery.MoviesWithMainActor.toMovieDetails() =
-    MovieDetails(
+fun MoviesWithMainActorsQuery.MoviesWithMainActor.toMovieItem() =
+    MovieItem(
         this.vote_count(),
         this.id(),
         this.video(),
@@ -39,3 +41,12 @@ fun MoviesWithMainActorsQuery.MoviesWithMainActor.toMovieDetails() =
         this.order(),
         this.profile_path()
     )
+
+fun MovieCastQuery.MovieCast.toCastItem() = CastItem(
+    this.cast_id(),
+    this.character(),
+    this.id(),
+    this.name(),
+    this.order(),
+    this.profile_path()
+)
